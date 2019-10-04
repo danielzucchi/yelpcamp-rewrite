4355220d7bf5b49d9f2c428c99939fd9f6425052
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2019-10-04 16:35:20 +0100

    Fixed service

--- api-services/hotelGetService.js
+++ api-services/hotelGetService.js
@@ -2,6 +2,8 @@ const Hotel = require("../models/hotelSchema")
 
 const hotelGetService = {
   findAllHotels: function() {
-    return Hotel.find({ deleted: false })
-  }.then(foundHotels => foundHotels)
+    return Hotel.find({ deleted: false }).then(foundHotels => foundHotels)
+  }
 }
+
+module.exports = hotelGetService
