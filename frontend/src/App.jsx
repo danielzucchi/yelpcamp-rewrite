8dc550d9c4912f65b80ecae1ddea5c46fd0c5f53
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2020-04-15 12:50:03 +0100

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
+import React, { Component } from "react"
+import axios from "axios"
+import "./App.css"
 
 class App extends Component {
   state = {
     hotels: []
-  };
+  }
 
   fetchHotels = () => {
     axios
-      .get('/hotels')
+      .get("/hotels")
       .then(res => {
-        console.log(res.data);
-        this.setState({ hotels: res.data });
+        console.log(res.data)
+        this.setState({ hotels: res.data })
       })
-      .catch(err => console.log(err));
-  };
+      .catch(err => console.log(err))
+  }
 
   render() {
     const hotels = this.state.hotels.map(hotel => (
@@ -25,19 +24,18 @@ class App extends Component {
         <h4>{hotel.price}</h4>
         <p>{hotel.description}</p>
       </div>
-    ));
+    ))
 
     return (
       <div>
-        <NavBar />
         <div className="App">
           <h1>HotelReviews</h1>
           <button onClick={this.fetchHotels}>Fetch</button>
           <div>{hotels}</div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
